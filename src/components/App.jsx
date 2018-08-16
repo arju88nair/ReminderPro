173aec2dfd6869337cbd52a7f614ecea5ba2e862
arju88nair <arju88nair@gmail.com>
2018-08-16 21:05:22 +0530

    Initial structure

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <div className="App">
+                <div className="title">Remind this fucker!</div>
+                <div className="form-inline">
+                    <div className="form-group">
+                        <input className="form-control" placeholder="Whatchya want to remind asshole?"></input>
+                    </div>
+                    <button className="btn btn-success" type="button">Submit me fucker!</button>
+
+                </div>
+            </div>
+        );
+    }
+
+
+}
